deda4705d91763663b9bc49546f2bd26fca6a2a8
Patel Harshad <harshadce2008@gmail.com>
2016-11-08 12:37:55 +0530

    second commit

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,6 @@
+ShopifyApp.configure do |config|
+  config.api_key = "e9144be479e81a67ff01fc2d490751a4"
+  config.secret = "c119e7681df2212861f5e97aa0502cfa"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
