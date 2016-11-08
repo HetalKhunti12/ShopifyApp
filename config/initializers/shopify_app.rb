544ada387e480e1f7ab7e06360456603d5547363
Patel Harshad <harshadce2008@gmail.com>
2016-11-08 13:10:24 +0530

    second commit

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,5 @@ ShopifyApp.configure do |config|
   config.api_key = "e9144be479e81a67ff01fc2d490751a4"
   config.secret = "c119e7681df2212861f5e97aa0502cfa"
   config.scope = "read_orders, read_products"
-  config.redirect_uri = "https://peaceful-scrubland-80434.herokuapp.com/auth/shopify/callback"
   config.embedded_app = true
 end
