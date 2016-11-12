d11113e98cea19d4b423dc54ce942015009ca673
Patel Harshad <harshadce2008@gmail.com>
2016-11-12 13:33:33 +0530

    add plans to heroku app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Plan.create(name: "Basic", amount: 9.00, active: true)
+Plan.create(name: "Standard", amount: 30.00, active: true)
+Plan.create(name: "Premium", amount: 50.00, active: true)
